ea6cf4a72b6db717f4dd940dea6b40cd849c1285
jhondominguez1 <jjjdomi@gmail.com>
2016-05-13 05:06:59 -0500

    adicione estilos css

---
+++ ova_prueba.php
@@ -0,0 +1,10 @@
+ El volumen de la unidad D es Particion_1
+ El n£mero de serie del volumen es: 0AAC-AFC3
+
+ Directorio de D:\local_ova_prueba
+
+13/05/2016  05:02 a. m.    <DIR>          .
+13/05/2016  05:02 a. m.    <DIR>          ..
+13/05/2016  05:02 a. m.                 0 ova_prueba.php
+               1 archivos              0 bytes
+               2 dirs  76.760.502.272 bytes libres
